206f876f60c6733a4eab982c1ba8f4a3413da524
RIN-1011 <60701386+RIN-1011@users.noreply.github.com>
2023-09-05 21:02:56 +0900

    [Gold V] Title: 호텔, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -13,26 +13,42 @@ public class Main {
 		int C = Integer.parseInt(st.nextToken()); //늘릴 고객 수
 		int N = Integer.parseInt(st.nextToken()); //도시의 개수
 		
-		int dp[] = new int[C+101]; //비용 저장 dp배열
-		//초기화
-		Arrays.fill(dp, 999999999);
-		dp[0] = 0;
+		int cost[] = new int[N+1]; //홍보 비용
+		int cus[] = new int[N+1]; //얻는 고객 수
 		
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			int cost = Integer.parseInt(st.nextToken()); //홍보 비용
-			int cus = Integer.parseInt(st.nextToken()); //얻는 고객 수
-			for (int j = cus; j < C+101; j++) {
-				//현재 dp에 저장된 값과 새로운 값 비교
-				dp[j] = Math.min(dp[j], dp[j-cus]+cost);
+			cost[i] = Integer.parseInt(st.nextToken());
+			cus[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int dp[][] = new int[N+1][C+1]; //비용 저장 dp배열
+		
+		for (int i = 0; i < N+1; i++) {
+			Arrays.fill(dp[i], 999999999);
+		}
+
+//		for (int i = 1; i <= 100; i++) {
+//			System.out.print(i + "\t");
+//		}
+//		System.out.println();
+//		System.out.println("----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+		for (int i = 1; i <= N; i++) {
+			for (int c = 1; c <= C; c++) {
+				if(c-cus[i]<=0) { //배열 범위 벗어나는 것 방지
+					dp[i][c] = Math.min(dp[i-1][c], cost[i]); //이미 고객 수가 넘치므로 현재 비용과 i-1 비용 비교
+				}
+				else {
+					dp[i][c] = Math.min(dp[i-1][c], Math.min(dp[i][c-cus[i]]+cost[i], dp[i-1][c-cus[i]]+cost[i]));
+				}
+				if(dp[i][c] == 0) { //min값이 0이 되는 것 방지
+					dp[i][c] = dp[i][c-cus[i]]+cost[i];
+				}
+//				System.out.print(dp[i][c] + "\t");
 			}
+//			System.out.println();
 		}
-		//적어도 C명의 고객 이상 확보했을 때 최소 비용 찾기
-		int result = Integer.MAX_VALUE;
-		for (int i = C; i < C+101; i++) {
-			result = Math.min(result, dp[i]);
-		}
-		System.out.println(result);
+		System.out.println(dp[N][C]);
 	}
 
 }
