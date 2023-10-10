d338eba89d1de26691a6d8d4da2672f409006228
RIN-1011 <60701386+RIN-1011@users.noreply.github.com>
2023-10-10 16:47:37 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 127 ms, Memory: 25,200 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -1,57 +1,63 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-//창용마을무리의개수
 public class Solution {
-	static int[] p; //루트 배열
+	static int N, M;
+	static int[] p; //루트 노드
 	
 	public static int find(int a) {
-		if(a==p[a]) { //루트까지 올라갔다면
-			return a; //루트 반환
+		//루트 노드 찾았을 경우 반환
+		if(p[a] == a) {
+			return a;
 		}
-		return p[a]=find(p[a]);
+		return p[a] = find(p[a]);
 	}
 	
 	public static boolean union(int a, int b) {
 		int pa = find(a);
 		int pb = find(b);
 		
-		if(pa==pb) { //이미 같은 그룹일 경우 false
+		//이미 같은 무리인 경우 패스
+		if(pa == pb) {
 			return false;
 		}
-		p[pa] = pb; //union (같은 무리로 묶기)
+		
+		p[pa] = pb; //같은 무리로 묶기
 		return true;
 	}
 	
-	public static void main(String[] args) throws IOException{
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine()); //테스트 케이스 개수
+		int T = Integer.parseInt(br.readLine()); //테케 개수
 		for (int t = 1; t <= T; t++) {
 			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken()); //마을에 사는 사람 수
-			int M = Integer.parseInt(st.nextToken()); //사람의 관계 수
+			N = Integer.parseInt(st.nextToken()); //사람 수
+			M = Integer.parseInt(st.nextToken()); //관계 수
 			
 			//make-set
 			p = new int[N+1];
-			for (int i = 1; i <= N; i++) {
-				p[i] = i;
+			for (int j = 1; j <= N; j++) {
+				p[j] = j;
 			}
 			
-			int cnt = N; //마을 사는 사람 수만큼 카운트 초기화 
+			int result = N;
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
-				//같은 그룹이 아니라면 무리에 포함하고 카운트 감소
-				if(union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))) {
-					cnt--;
+				//a, b 사람 서로 아는 존재
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				//같은 무리가 아니었다면 무리에 포함하고 카운트 감소
+				if(union(a, b)) {
+					result--;
 				}
 			}
-			sb.append("#").append(t).append(" ").append(cnt).append("\n");
+			sb.append("#").append(t).append(" ").append(result).append("\n");
 		}
 		System.out.println(sb);
-		br.close();
 	}
 
 }
