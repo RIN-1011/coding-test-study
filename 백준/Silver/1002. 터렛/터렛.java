dc0cbf4d6ed2d62d9a24c790a5de5ff38460a76a
RIN-1011 <60701386+RIN-1011@users.noreply.github.com>
2025-01-09 19:47:53 +0900

    [Silver III] Title: 터렛, Time: 108 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1002. 터렛/터렛.java
+++ 백준/Silver/1002. 터렛/터렛.java
@@ -8,41 +8,57 @@ public class Main {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		
-		int T = Integer.parseInt(br.readLine()); //테케 개수
-		for (int t = 1; t <= T; t++) {
+		int T = Integer.parseInt(br.readLine()); //테스트 케이스 개수
+		
+		for(int i=0; i<T; i++) {
 			st = new StringTokenizer(br.readLine());
-			//원1
+			
 			int x1 = Integer.parseInt(st.nextToken());
 			int y1 = Integer.parseInt(st.nextToken());
 			int r1 = Integer.parseInt(st.nextToken());
-			//원2
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			int r2 = Integer.parseInt(st.nextToken());
 			
-			//중심거리
-			int d = (int)(Math.pow((x1-x2),2)+Math.pow((y1-y2),2));
-
-			//두 원의 위치 관계
+			//[두 원 사이의 위치 관계]
+			//두 좌표 거리 계산
+			int d = (int)(Math.pow(x1-x2, 2)+Math.pow(y1-y2, 2));
 			
-			//중심 좌표 같고 반지름도 같을 경우
+			//1.무한대 점에서 만나는 경우
 			if(x1==x2 && y1==y2 && r1==r2) {
 				sb.append(-1).append("\n");
 			}
-			//한 점에서 만날 경우 (외접, 내접)
-			else if(Math.pow(r1+r2,2)==d || Math.pow(r1-r2, 2)==d) {
-				sb.append(1).append("\n");
-			}
-			//만나지 않을 경우(외부, 내부)
-			else if(Math.pow(r1+r2,2)<d || Math.pow(r1-r2, 2)>d) {
+			
+			//2.만나지 않는 경우
+			//2-1)외부에서 만나지 않을 때
+			else if(d>Math.pow(r1+r2, 2)) {
 				sb.append(0).append("\n");
 			}
-			//두 점에서 만날 경우
+			//2-2)내부에서 만나지 않을 때
+			else if(d<Math.pow(r1-r2,2)) {
+				sb.append(0).append("\n");
+			}
+			//2-3)동심원일 때
+			else if(d==0 && r1!=r2) {
+				sb.append(0).append("\n");
+			}
+			
+			//3.한 점에서 만나는 경우
+			//3-1)외접할 때
+			else if(d==Math.pow(r1+r2,2)) {
+				sb.append(1).append("\n");
+			}
+			//3-2)내접할 때
+			else if(d==Math.pow(r1-r2,2)) {
+				sb.append(1).append("\n");
+			}
+			
+			//4.두 점에서 만나는 경우
 			else {
 				sb.append(2).append("\n");
 			}
 		}
-		System.out.println(sb);
+		System.out.print(sb);
 	}
 
 }
