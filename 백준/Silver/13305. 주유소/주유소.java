ba8e5b6781636df186ce1ae8c9cd8d7709495f57
RIN-1011 <60701386+RIN-1011@users.noreply.github.com>
2025-01-18 18:54:50 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 30532 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,36 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-
-		int N = Integer.parseInt(br.readLine()); //도시의 개수
-		//도로 길이
+		
+		int N = Integer.parseInt(br.readLine()); //도시 개수
+		
+		int[] len = new int[N-1]; //도로의 길이
 		st = new StringTokenizer(br.readLine());
-		Queue<Integer> length = new LinkedList<>();
-		for (int i = 0; i < N-1; i++) {
-			length.add(Integer.parseInt(st.nextToken()));
-		}
-		//각 도시 주유소 리터당 가격
-		st = new StringTokenizer(br.readLine());
-		Queue<Integer> cost = new LinkedList<>();
-		for (int i = 0; i < N; i++) {
-			cost.add(Integer.parseInt(st.nextToken()));
+		for(int i=0; i<N-1; i++) {
+			len[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int result = 0; //최소 비용
-		while(!length.isEmpty()) {
-			int nowCost = cost.remove(); //가장 왼쪽 도시
-			result += nowCost*length.remove(); //거리만큼 기름 충전
-			//현재 비용이 그 다음 도시 비용보다 싸다면 현재 도시에서 기름 충전하기
-			while(cost.peek()>nowCost) {
-				result += nowCost*length.remove();
-				cost.remove();
-			}
+		int[] price = new int[N]; //리터당 가격
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			price[i] = Integer.parseInt(st.nextToken());
 		}
+		
+		int min = price[0]; //최소 비용 초기화
+		long result = 0; //총 최소 비용
+		
+		for(int i=0; i<N-1; i++) { //최소 비용 계산
+			//현재 최소 비용보다 더 쌀 경우 갱신
+			if(min > price[i]) {
+				min = price[i];
+			}
+			result += min * len[i];
+		}
+		
 		System.out.println(result);
 	}
 
