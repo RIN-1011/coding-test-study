5c04c7186eb4aeb6fb3b6047668d7ae564820ad6
RIN-1011 <60701386+RIN-1011@users.noreply.github.com>
2025-01-22 21:31:09 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 15696 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,43 +1,41 @@
 import java.util.*;
 import java.io.*;
 
-//N과 M(2)
 public class Main {
+	static StringBuilder sb;
 	static int N, M;
-	static int numbers[];
-	static int input[];
+	static int[] number; //완성한 수열
 	
-	public static void algorithm(int cnt, int start) {
-		if(cnt == M) { //수열 다 만들어졌을 경우
-			for(int n:numbers) {
-				System.out.print(n + " "); //결과 출력
+	static void comb(int cnt, int start) { //조합
+		//조합 완성
+		if(cnt == M) {
+			for(int i=0; i<M; i++) {
+				sb.append(number[i]+" ");
 			}
-			System.out.println();
+			sb.append("\n");
 		}
 		else {
-			for (int i = start; i <= N; i++) { //중복없이 하기 위해 수열 내 오름차순 지키기
-				numbers[cnt] = input[i]; //cnt 인덱스에 자연수 넣기
-				algorithm(cnt+1, i+1); //자리수 밀기, 이전 수들보다 큰 수 넣기
+			for(int i=start; i<N; i++) {
+				number[cnt] = i+1;
+				comb(cnt+1, i+1);
 			}
 		}
 	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st;
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+		sb = new StringBuilder();
 		
-		input = new int[N+1]; //1~N까지 수
-		numbers = new int[M]; //조합 개수
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); //자연수
+		M = Integer.parseInt(st.nextToken()); //선택 개수
 		
-		//1~N 저장
-		for (int i = 1; i <= N; i++) {
-			input[i] = i;
-		}
+		number = new int[M]; //초기화
 		
-		algorithm(0, 1);
-
+		comb(0, 0);
+		
+		System.out.println(sb);
 	}
-
 }
